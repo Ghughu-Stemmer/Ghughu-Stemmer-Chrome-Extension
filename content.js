4f1b3016d0cca7d1dba9131580cce7a9ba150ece
Rajan-sust <imrajon0@gmail.com>
2019-04-24 16:44:22 +0600

    updating content.js

--- content.js
+++ content.js
@@ -1,23 +1,65 @@
 chrome.runtime.onMessage.addListener(receiver);
 
+
+// Checking Unicode Value
+
+
+function isBanglaWord(word) {
+
+	for (var index = 0; index < word.length; index++) {
+		var val = word.charCodeAt(index);
+
+		if(val > 2559 || val < 2458) {
+			return false;
+		}
+	}
+
+	return true;
+
+
+}
+
+function getWithoutStopMark(word) {
+
+	var stopMarks = ["!",",","’","?",";","।",":"];
+
+	if(word.startsWith("‘")) {
+		word = word.slice(1,);
+	}
+
+	for(ch of stopMarks) {
+		if(word.endsWith(ch)){
+			return word.slice(0,-1);
+		}
+	}
+
+	return word;
+}
+
+
+
+
+
+
+
 function receiver(request, sender, sendResponse) {
 	if (request.message === "user clicked!") {
 		
 	  	var textContent = document.body.innerText;
-	  	var words = new Set(textContent.split(" "));
-		var vervs = "";
+	  	var splittedWords = textContent.split(/[\t\n (){}]/);
 
-		for(word of words) {
-			if(word.endsWith("।")) {
-				word = word.slice(0,-1);
-				if (window.confirm(word)) {
-					vervs+= word + " ";
-				}
-			}
-		}
-		console.log(vervs);
-    
-  }
+	  	var shapedWord = new Set();
+	  	
+
+	  	for(word of splittedWords) {
+	  		word = word.trim();
+
+	  		if(isBanglaWord(word) && word) {
+	  			shapedWord.add(getWithoutStopMark(word));
+	  		}
+	  	}
+	  	console.log(shapedWord);
+	}
 }
 
 
