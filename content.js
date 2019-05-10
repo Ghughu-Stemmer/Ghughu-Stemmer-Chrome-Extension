2a07c0d4b0f7ef515ff710fb62963988aa165618
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2019-05-10 22:16:40 +0600

    added auto interval and changed api endpoint and permissions

--- content.js
+++ content.js
@@ -13,44 +13,66 @@ $(function () {
 		return true;
 	}
 
+	var uploadedWords = [];
+	var intervals = [];
 
-	function receiver(request, sender, sendResponse) {
-		if (request.message === "user clicked!") {
-			var textContent = document.body.innerText;
-			var splittedWords = textContent.split(/[ \t\n,!'"`’;।:={}\[\]\\\^\$\.\|\?\*\+\(\)১২৩৪৫৬৭৮৯০]/);
+	function getCallback(w) {
+		console.log(`callback for ${w}`);
+		let myWord = w;
+		var func = function (data, textStatus, jQxhr) {
+			console.log(`Upload Successful`);
+			uploadedWords.push(myWord);
+			console.log(myWord);
+		}
+		return func;
+	}
 
-			var shapedWords = new Set();
+	function run() {
+		console.log(`Uploaded Words: ${uploadedWords}`);
+		var textContent = document.body.innerText;
+		var splittedWords = textContent.split(/[ \t\n,!'"`’;।:={}\[\]\\\^\$\.\|\?\*\+\(\)১২৩৪৫৬৭৮৯০]/);
 
-			for (word of splittedWords) {
-				word = word.trim();
-				if (isBanglaWord(word) && word) {
-					shapedWords.add(word);
+		var shapedWords = new Set();
+
+		for (word of splittedWords) {
+			word = word.trim();
+			if (isBanglaWord(word) && word) {
+				shapedWords.add(word);
+			}
+		}
+
+		var uniqueWords = [...shapedWords];
+		console.log(uniqueWords);
+
+		for (word of uniqueWords) {
+			if (uploadedWords.includes(word)) continue;
+
+			let data = {
+				"word": word
+			};
+
+			$.ajax({
+				url: server + '/api/words/insertWord',
+				type: 'POST',
+				contentType: 'application/json; charset=utf-8',
+				data: JSON.stringify(data),
+				success: getCallback(word),
+				error: function (jqXhr, textStatus, errorThrown) {
+					console.log("Upload Failed");
 				}
-			}
-
-			var uniqueWords = [...shapedWords];
-			console.log(uniqueWords);
-
-			for (word of uniqueWords) {
-				let data = {
-					"word": word
-				};
-
-				$.ajax({
-					url: server + '/api/words',
-					type: 'POST',
-					contentType: 'application/json; charset=utf-8',
-					data: JSON.stringify(data),
-					success: function (data, textStatus, jQxhr) {
-						console.log("Upload Successful");
-					},
-					error: function (jqXhr, textStatus, errorThrown) {
-						console.log("Upload Failed");
-					}
-				});
-
-			}
+			});
 
 		}
 	}
+
+
+	function receiver(request, sender, sendResponse) {
+		if (request.message === "user clicked!") {
+			console.log("clicked");
+			run();
+		}
+	}
+
+	let interval = setInterval(run, 5000);
+	intervals.push(interval);
 })
